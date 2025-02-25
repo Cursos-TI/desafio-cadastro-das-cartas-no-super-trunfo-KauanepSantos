3655bf77417fe642d71ef473fec0674551ca20aa
KauanepSantos <kpssantos91@gmail.com>
2025-02-25 17:25:47 +0000

    encerrando programa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -34,3 +34,5 @@ printf("População: %d\n", populacao2);
 printf("Área: %.2f km²\n", area2);
 printf("PIB: %.2f bilhões de dólares canadenses\n", pib2);
 printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
+return 0;
