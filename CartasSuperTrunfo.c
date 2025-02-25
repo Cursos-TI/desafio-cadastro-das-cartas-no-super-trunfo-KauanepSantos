0782245951d9fa884fb681039925672c6d2528f3
KauanepSantos <kpssantos91@gmail.com>
2025-02-25 17:09:17 +0000

    Alterando dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
-char cidade1 = "O"; 
-char codigo1[] = 001 ;
+char provincia1 = "O"; 
+char codigo1[] = O01 ;
 char nomecidade1 [] = "Toronto" ;
 int populacao1 = 2930000;
 float area1 = 630.2 ;
 float pib1 =332.0;
 int pontosturisticos1 = 25;
+
+char provincia2 = 'Q';
+char codigo2 =
